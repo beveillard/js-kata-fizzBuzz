1d6a078f129f51a5c713aa6dd48e4d99a7f33997
Benoît Eveillard <benoit.eveillard@gmail.com>
2020-12-30 18:17:43 -0500

    refactor(FizzBuzz) clarifying isMultipleOf

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 export function fizzBuzz(n) {
   let result = "";
 
-  if (n % 3 == 0) result = "Fizz";
-  if (n % 5 == 0) result += "Buzz";
+  if (isMultipleOf(3, n)) result = "Fizz";
+  if (isMultipleOf(5, n)) result += "Buzz";
   if (result === "") result += n;
 
   return result;
+}
+
+function isMultipleOf(x, n) {
+  return n % x == 0;
 }
