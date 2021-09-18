2f7490b4c6120738d80e203e94bed5e7d52fc2ed
Benoît Eveillard <benoit.eveillard@gmail.com>
2021-09-18 09:02:03 -0400

    FizzBuzz

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export function systemUnderTest(): boolean {
-  return true;
+export function fizzBuzz(n: number): string {
+  let result = "";
+  if (isMultipleOf(3, n)) result = "Fizz";
+  if (isMultipleOf(5, n)) result += "Buzz";
+
+  return (result === "") ? String(n) : result;
+}
+
+function isMultipleOf(m: number, n: number): boolean {
+  return n % m === 0;
 }
